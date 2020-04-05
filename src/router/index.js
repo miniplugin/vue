a874dfc02ba87a27c6b35d19e4a189745fa21991
kim ilguk <boramcom@daum.net>
2020-04-05 17:17:24 +0900

    라우트 테스트.

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '*',
+    name: 'E404',
+    component: () => import(/* E404 File Not Found */ '../views/E404.vue')
   }
 ]
 
---
+++ src/views/E404.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>404 파일을 찾을 수 없습니다.</h1>
+  </div>
+</template>
