5c7324873a920b2579d6a5a72f432775662db4a5
kim ilguk <boramcom@daum.net>
2020-04-07 18:48:17 +0900

    hidden-sm-and-down css클리스 사용.

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
         <v-toolbar-title>Title</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn>
-          <router-link to="/">Home</router-link>|
-        </v-btn>
-        <v-btn>
-          <router-link to="/about">About</router-link>
-        </v-btn>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn>
+            <router-link to="/">Home</router-link>
+          </v-btn>
+          <v-btn>
+            <router-link to="/about">About</router-link>
+          </v-btn>
+        </v-toolbar-items>
         <v-btn icon>
           <v-icon>mdi-dots-vertical</v-icon>
         </v-btn>
