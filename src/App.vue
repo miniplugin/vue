c26eea076de37bdee03f095efb9b141bf33d6163
kim ilguk <boramcom@daum.net>
2020-04-08 16:20:57 +0900

    토글 메뉴 만들기.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <!--
+        속성 app: 화면크기에 맞게 네비크기 조정 속성
+        속성 v-model: 화면에 토글 액션으로 보여주기/숨기기 drawer(true/false)
+      -->
+      <v-toolbar
+        flat
+        color="trasparent"
+      >
+        <v-toolbar-title>메뉴</v-toolbar-title>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item
+          v-for="(item,index) in items"
+          v-bind:key="index"
+          v-bind:to="item.url"
+        >
+          <v-list-item-avatar>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn icon>
+              <v-icon color="grey lighten-1">mdi-information</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-card>
       <v-toolbar color="lime">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        <v-toolbar-title>Title</v-toolbar-title>
+        <!-- v-on:click="test" = @click="test" -->
+        <v-app-bar-nav-icon @click="navDrawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>Vue 앱</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn>
@@ -25,20 +62,33 @@
 </template>
 <script>
 export default {
+  name: 'App',
   data () {
     return {
+      drawer: false,
+      items: [ // javascript object = 2차원배열 형태 = [{key:value},{연관배열},{json형태}...]
+        {
+          icon: 'mdi-home',
+          title: 'Home',
+          url: '/'
+        },
+        {
+          icon: 'mdi-information-outline',
+          title: 'About',
+          url: '/about'
+        }
+      ]
+    }
+  },
+  methods: {
+    navDrawer () {
+      console.log('navDrawer 클릭')
+      this.drawer = !this.drawer
     }
   }
 }
 </script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 .v-btn--contained {
   margin-right: 30px;
 }
